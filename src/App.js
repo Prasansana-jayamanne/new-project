bfd101b6345f76569e6f22ed850adb4bca053218
Prasansana-jayamanne <74807699+Prasansana-jayamanne@users.noreply.github.com>
2020-11-24 17:01:19 +0530

    initial commit on react file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+
+import Home from './components/Home'
+
+ 
+function App() {
+  return (
+    <div className="App">
+      <h1>React Firebase App</h1>
+      <Home/>
+     
+    </div >
+  );
+}
+
+export default App;
